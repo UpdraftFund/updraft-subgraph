19a875eb2a8a0c102f25023b59fb61cf6c6b1726
adamstallard <205792+adamstallard@users.noreply.github.com>
2024-12-25 03:49:52 -0800

    Don't index Stake as an entity. We can read the stake for a single user from the smart contract.

--- src/solution.ts
+++ src/solution.ts
@@ -1,5 +1,5 @@
 import { store, crypto, BigInt, Bytes } from '@graphprotocol/graph-ts'
-import { Solution, User, SolutionContribution, Stake } from "../generated/schema"
+import { Solution, User, SolutionContribution } from "../generated/schema"
 import {
   Contributed,
   GoalExtended,
@@ -108,42 +108,9 @@ export function handleStakeUpdated(event: StakeUpdated): void {
   let solution = Solution.load(event.address)!;
   solution.stake = event.params.totalStake;
   solution.save();
-
-  let staker = User.load(event.params.addr);
-  if (!staker) {
-    staker = new User(event.params.addr);
-    staker.save();
-  }
-
-  let stake = new Stake(stakeId(event.address, event.params.addr));
-  stake.staker = event.params.addr;
-  stake.solution = event.address;
-  stake.amount = event.params.stake;
-  stake.save();
-}
-
-export function handleStakeTransferred(event: StakeTransferred): void {
-  store.remove('Stake', stakeId(event.address, event.params.from).toHex());
-
-  let staker = User.load(event.params.to);
-  if (!staker) {
-    staker = new User(event.params.to);
-    staker.save();
-  }
-
-  let stakeTo = new Stake(stakeId(event.address, event.params.to));
-  stakeTo.staker = event.params.to;
-  stakeTo.solution = event.address;
-  stakeTo.amount = event.params.newStake;
-  stakeTo.save();
 }
 
 function contributionId(solutionAddress: Bytes, userAddress: Bytes, positionIndex: BigInt): Bytes {
   const concatBytes = solutionAddress.concat(userAddress).concatI32(positionIndex.toI32());
-  return Bytes.fromByteArray(crypto.keccak256(concatBytes));
-}
-
-function stakeId(solutionAddress: Bytes, userAddress: Bytes): Bytes {
-  const concatBytes = solutionAddress.concat(userAddress);
   return Bytes.fromByteArray(crypto.keccak256(concatBytes));
 }
